ae1da2afd6d7fce0a0e3f20c629dd1efdb4fdcd4
Suraj Sood <surajsoodx@gmail.com>
2023-10-03 10:08:30 -0700

    Add files via upload

---
+++ CS AP - for looping.py
@@ -0,0 +1,21 @@
+# Online Python - IDE, Editor, Compiler, Interpreter
+for num in range(6):
+    print(num)
+for letter in "pythonista":
+    print(letter)
+for x in range(11):
+    print(x)
+for char in "kaleidoscope":
+    print(char)
+for num in range(50,66,1):
+    print(num)
+for num in range(50,66,3):
+    print(num)
+for num in range(1,53,2):
+    print(num)
+for num in range(2,102,2):
+    print(num)
+for num in range(10,-1,-1):
+    print(num)
+for num in range(10,1010,10):
+    print(num)
